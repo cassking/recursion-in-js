fe5e3889715ea10d4d2aaad7bec5d41c40076bbb
Cassandra King <puddincat@gmail.com>
2018-04-22 19:15:43 -0400

    update

--- recursion.js
+++ recursion.js
@@ -10,7 +10,44 @@ function factorializeNumber(number) {
   }
 
 //factorializeNumber(67)
-console.log(factorializeNumber(20))
+console.log('factorial:__', factorializeNumber(20))
 //5 should return 120
 //10 should return 3628800
 //20 should return 2432902008176640000
+
+///Write a JavaScript program to
+//find the greatest common divisor
+//(gcd) of two positive numbers
+//euclids algorithm:
+//remainder = max % min
+//If the remainder is equal to 0, you can stop because you have identified
+//the GCD which is the smaller number from the previous step.
+//Start back at step one, using the smaller number as
+//the larger number and the remainder as the smaller number:
+//max = min
+//min = remainder
+let maxNum, minNum, remainder;
+let numArray = []
+//Divide the larger number by the small one.
+function greatestCommDiv(number1, number2) {
+  if(number1> number2){
+    maxNum = number1
+  }
+  if (number2> number1){
+    maxNum = number2
+  }
+  if(remainder === 0) {
+    return minNum
+  }
+    remainder = maxNum%minNum
+
+
+
+    maxNum = minNum
+    remainder = minNum
+
+    greatestCommDiv(maxNum, maxNum%minNum)
+
+
+}
+greatestCommDiv(60,72)
